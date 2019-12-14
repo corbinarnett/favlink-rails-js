ff20326bbf9cd04e1524cbe27691a052716e79db
corbinarnett <corbin.c.arnett@gmail.com>
2019-12-14 01:26:40 -0800

    render lists in dropdown form

--- client/src/components/lists.js
+++ client/src/components/lists.js
@@ -2,7 +2,12 @@ class Lists {
   constructor() {
     this.lists = [];
     this.adapter = new ListsAdapter()
+    this.initBindingsAndEventListeners();
     this.fetchAndLoadLists()
+  }
+
+  initBindingsAndEventListeners() {
+    this.dropdownContainer = document.querySelector(".form-group #ListControlSelect")
   }
 
   fetchAndLoadLists() {
@@ -10,5 +15,22 @@ class Lists {
       lists.forEach(list => this.lists.push(new List(list)))
       
     })
+    .then(() => {
+      this.render();
+    })
+  }
+
+  render() {
+    this.lists.forEach(list => this.listInput(list))
+  }
+
+  listHtml(list) {
+    return `
+    <option>${list.title}</option>
+    `
+  }
+
+  listInput(list) {
+    this.dropdownContainer.innerHTML += this.listHtml(list)
   }
 }
