a7b3cc466eb14803862807123177324bb275d752
corbinarnett <corbin.c.arnett@gmail.com>
2019-12-13 21:10:02 -0800

    define app, website, and service classes

--- client/src/services/WebsitesService.js
+++ client/src/services/WebsitesService.js
@@ -2,4 +2,8 @@ class WebsitesService {
   constructor() {
     this.baseUrl = "http://localhost:3000/api/v1/websites"
   }
+
+  getWebsites() {
+    return fetch(this.baseUrl).then(res => console.log(res))
+  }
 }
