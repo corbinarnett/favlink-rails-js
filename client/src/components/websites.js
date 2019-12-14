1166df66d14f318c08510d6d680946a19c60c248
corbinarnett <corbin.c.arnett@gmail.com>
2019-12-14 00:19:04 -0800

    render saved websites from db

--- client/src/components/websites.js
+++ client/src/components/websites.js
@@ -1,24 +1,40 @@
 class Websites {
   constructor() {
-    this.websites = []
-    this.adapter = new WebsitesAdapter()
+    this.websites = [];
+    this.adapter = new WebsitesAdapter();
     // this.bindEventListeners()
-    this.fetchAndLoadWebsites()
+    this.fetchAndLoadWebsites();
   }
 
   fetchAndLoadWebsites() {
-    this.adapter.getWebsites().then(websites => {
-      websites.forEach(website => this.websites.push(new Website(website)))
-      console.log(this.websites)
-    })
-    .then(() => {
-      this.render()
-    })
+    this.adapter
+      .getWebsites()
+      .then(websites => {
+        websites.forEach(website => this.websites.push(new Website(website)));
+        // console.log(this.websites)
+      })
+      .then(() => {
+        this.render();
+      });
   }
 
   render() {
-    const websiteContainer = document.getElementById("savedSite-container")
-    websiteContainer.innerHTML = "My Saved Websites Here!"
-    console.log("my saved sites are", this.websites)
+    this.websites.forEach(website => this.displayCard(website))
+  }
+
+  websiteCard(website) {
+    return `
+    <div class="card bg-light">
+      <div class="card-body">
+        <h3>${website.title}</h3>
+        <a class="btn btn-default"  target="_blank" href="${website.link} role="button">Visit</a>
+      </div>
+    </div>
+    `
+  }
+
+  displayCard(website) {
+    const linkContainer = document.getElementById("savedSite-container");
+    linkContainer.innerHTML += this.websiteCard(website)
   }
 }
