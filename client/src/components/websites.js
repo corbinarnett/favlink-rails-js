50fce8cfccc50850022406761c714a846d04a33a
corbinarnett <corbin.c.arnett@gmail.com>
2019-12-15 18:53:16 -0800

    take out commented code

--- client/src/components/websites.js
+++ client/src/components/websites.js
@@ -72,33 +72,4 @@ class Websites {
       .join("");
   }
 
-  // render() {
-  //   this.linkContainer.innerHTML = this.websites
-  //     .map(website => { return `
-  //     <div class="card bg-light">
-  //       <div class="card-header">
-  //         ${website.listTitle}
-  //       </div>
-  //       <div class="card-body">
-  //         <h3 value="${website.id}">${website.title}</h3>
-  //         <button class="btn btn-primary btn-small" onclick="window.open('${website.link}', '_blank')">Visit</button>
-  //         <button onclick="Websites.deleteFromCard(${website.id})" class="btn btn-danger btn-small">Delete</button>
-  //       </div>
-  //     </div></br>
-  //     ` })
-  //     .join("");
-  // }
-
-  // static deleteFromCard(id) {
-  //   alert(id)
-  //   // debugger
-  //   return fetch("http://localhost:3000/api/v1/websites" + "/" + id, {
-  //         method: 'delete',
-  //         headers: {
-  //           'Accept': 'application/json',
-  //           'content-type': 'application/json',
-  //         },
-  //       }).then(res => console.log(res))
-  
-  // }
 }
